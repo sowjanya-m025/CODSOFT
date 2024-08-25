52ad83b8415344fbd29372bc124fa2b0a3dae057
sowjanya-m025 <sowjanyamurgesh04@gmail.com>
2024-08-25 19:43:02 +0530

    Create Simple Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -13,8 +13,6 @@ def Calculator(n1, n2, operation):
         return n1/n2
     else:
         return "Error: Invalid operation."
-
-
 print("calculator") # Prompt the user for input
 n1= float(input("Enter first number: "))
 n2 = float(input("Enter second number: "))
